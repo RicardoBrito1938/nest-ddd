52109473c8a743831065f800b58618d42413fd35
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-12-04 15:04:16 +0000

    config: init project

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, HttpCode, Post } from "@nestjs/common";
+import type { PrismaService } from "src/prisma/prisma.service";
+
+@Controller("/accounts")
+export class CreateAccountController {
+	constructor(private prisma: PrismaService) {}
+
+	@Post()
+	@HttpCode(201)
+	async handle() {
+		const name = "John Doe";
+		const email = "ßjohndoe@mail.com";
+		const password = "123456";
+
+		await this.prisma.user.create({
+			data: {
+				name,
+				email,
+				password,
+			},
+		});
+	}
+}
