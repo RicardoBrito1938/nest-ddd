c21afb536609ccac2c191dd54295d5502f7ad993
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-12-06 11:57:23 +0000

    feat: add current user decorator and update JWT strategy for enhanced user context in question creation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { TokenPayload } from "src/auth/jwt.strategy";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Controller("/questions")
@@ -9,7 +11,8 @@ export class CreateQuestionController {
 	constructor(private prisma: PrismaService) {}
 
 	@Post()
-	async handle() {
+	async handle(@CurrentUser() user: TokenPayload) {
+		console.log(user);
 		return "ok";
 	}
 }
