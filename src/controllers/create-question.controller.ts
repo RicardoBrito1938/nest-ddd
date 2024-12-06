730c0a9d3ddab80dec4c48c3877fb7ad3316b0a6
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-12-06 12:45:38 +0000

    feat: implement question creation with validation and remove unused userId column

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { TokenPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createQuestionBodySchema = z.object({
+	title: z.string(),
+	content: z.string(),
+});
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -11,8 +22,34 @@ export class CreateQuestionController {
 	constructor(private prisma: PrismaService) {}
 
 	@Post()
-	async handle(@CurrentUser() user: TokenPayload) {
-		console.log(user);
-		return "ok";
+	async handle(
+		@CurrentUser() user: TokenPayload,
+		@Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+	) {
+		const { content, title } = body;
+		const { sub: userId } = user;
+
+		const slug = this.convertToSlug(title);
+
+		await this.prisma.question.create({
+			data: {
+				content,
+				title,
+				slug,
+				authorId: userId,
+			},
+		});
+	}
+
+	private convertToSlug(text: string): string {
+		return (
+			text
+				.toLowerCase()
+				.normalize("NFD")
+				// biome-ignore lint/suspicious/noMisleadingCharacterClass: <explanation>
+				.replace(/[\u0300-\u036f]/g, "")
+				.replace(/ /g, "-")
+				.replace(/[^\w-]+/g, "")
+		);
 	}
 }
