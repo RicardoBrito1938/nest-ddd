bd8a4458875eee267489148abcf8b182b25a5415
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-12-16 15:40:56 +0000

    feat: implement CRUD operations for answer and question comments and attachments in Prisma repositories

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -1,6 +1,7 @@
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
 import { Injectable } from "@nestjs/common";
+import { PrismaQuestionAttachmentMapper } from "../mappers/prisma-question-attachment-mapper";
 import { PrismaService } from "../prisma.service";
 
 @Injectable()
@@ -9,10 +10,23 @@ export class PrismaQuestionAttachmentsRepository
 {
 	constructor(private prisma: PrismaService) {}
 
-	findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]> {
-		throw new Error("Method not implemented.");
+	async findManyByQuestionId(
+		questionId: string,
+	): Promise<QuestionAttachment[]> {
+		const questionAttachments = await this.prisma.attachment.findMany({
+			where: {
+				questionId,
+			},
+		});
+
+		return questionAttachments.map(PrismaQuestionAttachmentMapper.toDomain);
 	}
-	deleteManyByQuestionId(questionId: string): Promise<void> {
-		throw new Error("Method not implemented.");
+
+	async deleteManyByQuestionId(questionId: string): Promise<void> {
+		await this.prisma.attachment.deleteMany({
+			where: {
+				questionId,
+			},
+		});
 	}
 }
--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -1,6 +1,7 @@
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 import { Injectable } from "@nestjs/common";
+import { PrismaQuestionCommentMapper } from "../mappers/prisma-question-comment-mapper";
 import { PrismaService } from "../prisma.service";
 
 @Injectable()
@@ -9,19 +10,54 @@ export class PrismaQuestionCommentsRepository
 {
 	constructor(private prisma: PrismaService) {}
 
-	findById(questionCommentId: string): Promise<QuestionComment | null> {
-		throw new Error("Method not implemented.");
+	async findById(questionCommentId: string): Promise<QuestionComment | null> {
+		const questionComment = await this.prisma.comment.findUnique({
+			where: {
+				id: questionCommentId,
+			},
+		});
+
+		if (!questionComment) {
+			return null;
+		}
+
+		return PrismaQuestionCommentMapper.toDomain(questionComment);
 	}
-	findManyByQuestionId(questionId: string): Promise<QuestionComment[]> {
-		throw new Error("Method not implemented.");
+
+	async findManyByQuestionId(questionId: string): Promise<QuestionComment[]> {
+		const questionComments = await this.prisma.comment.findMany({
+			where: {
+				questionId,
+			},
+		});
+
+		return questionComments.map(PrismaQuestionCommentMapper.toDomain);
 	}
-	create(questionComment: QuestionComment): Promise<void> {
-		throw new Error("Method not implemented.");
+
+	async create(questionComment: QuestionComment): Promise<void> {
+		const data = PrismaQuestionCommentMapper.toPersistence(questionComment);
+
+		await this.prisma.comment.create({
+			data,
+		});
 	}
-	delete(questionComment: QuestionComment): Promise<void> {
-		throw new Error("Method not implemented.");
+
+	async delete(questionComment: QuestionComment): Promise<void> {
+		await this.prisma.comment.delete({
+			where: {
+				id: questionComment.id.toString(),
+			},
+		});
 	}
-	update(questionComment: QuestionComment): Promise<void> {
-		throw new Error("Method not implemented.");
+
+	async update(questionComment: QuestionComment): Promise<void> {
+		const data = PrismaQuestionCommentMapper.toPersistence(questionComment);
+
+		await this.prisma.comment.update({
+			where: {
+				id: questionComment.id.toString(),
+			},
+			data,
+		});
 	}
 }
