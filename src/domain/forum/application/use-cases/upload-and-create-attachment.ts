b95ea3a43d94689813975e5b824cfe047390d20c
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-12-21 16:57:43 +0000

    feat: implement UploadAndCreateAttachment use case and repository; add attachment entity and error handling

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.ts
@@ -0,0 +1,54 @@
+import { type Either, left, right } from "@/core/either";
+import { Injectable } from "@nestjs/common";
+import { Attachment } from "../../enterprise/entities/attachment";
+import { AttachmentsRepository } from "../repositories/attachments-repository";
+import { Uploader } from "../storage/uploader";
+import { InvalidAttachmentTypeError } from "./errors/invalid-attachment-type-error";
+
+interface UploadAndCreateAttachmentUseCaseRequest {
+	fileName: string;
+	fileType: string;
+	body: Buffer;
+}
+
+type UploadAndCreateAttachmentUseCaseResponse = Either<
+	InvalidAttachmentTypeError,
+	{
+		attachment: Attachment;
+	}
+>;
+
+@Injectable()
+export class UploadAndCreateAttachmentUseCase {
+	constructor(
+		private attachmentsRepository: AttachmentsRepository,
+		private uploader: Uploader,
+	) {}
+
+	async execute({
+		body,
+		fileName,
+		fileType,
+	}: UploadAndCreateAttachmentUseCaseRequest): Promise<UploadAndCreateAttachmentUseCaseResponse> {
+		if (![".png", ".jpg", ".jpeg", ".pdf"].includes(fileType)) {
+			return left(new InvalidAttachmentTypeError(fileName));
+		}
+
+		const { url } = await this.uploader.upload({
+			fileName,
+			fileType,
+			body,
+		});
+
+		const attachment = Attachment.create({
+			title: fileName,
+			url,
+		});
+
+		await this.attachmentsRepository.create(attachment);
+
+		return right({
+			attachment,
+		});
+	}
+}
